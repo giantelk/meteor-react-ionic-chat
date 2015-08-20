ce06204837ace51db93bc4d6acb74908b1f0944f
Giant Elk <flyinghorsedancing@gmail.com>
2015-08-20 19:28:42 -0400

    Lots of refactoring

--- client/components/AppBody.jsx
+++ client/components/AppBody.jsx
@@ -1,23 +1,5 @@
-let Transition = React.addons.CSSTransitionGroup;
-
 AppBody = React.createClass({
-  getInitialState() {
-    return {
-      modal: false
-    }
-  },
-  ionModal(tab, content) {
-    this.setState({
-      modal: (
-        <IonModal modalContent={content}>
-          <div className="h1 title">{tab}</div>
-          <button onClick={ () => this.setState({modal:false}) } className="button button-icon active">
-            <i className="icon ion-ios-close-empty"></i>
-          </button>
-        </IonModal>
-      )
-    })
-  },
+
   render() {
     let subheader = window.location.pathname == "/other" ? "has-subheader" : ""
     let classes = "content overflow-scroll has-header "+ subheader
@@ -29,17 +11,10 @@ AppBody = React.createClass({
           <ReactRouter.Link className="button button-icon icon ion-heart" to={"/other"}></ReactRouter.Link>
         </div>
 
-        {this.state.modal ? <Backdrop /> : false}
-        <Transition transitionName="modal">
-          {this.state.modal}
-        </Transition>
-
-        {window.location.pathname == "/other" ? <ConversationSubheader ionModal={this.ionModal} /> : false}
-
         <div className="view">
           <div className="scroll-content ionic-scroll">
             <div className={classes}>
-              <ReactRouter.RouteHandler ionModal={this.ionModal} />
+              <ReactRouter.RouteHandler />
             </div>
           </div>
         </div>
--- client/components/Other.jsx
+++ client/components/Other.jsx
@@ -1,20 +1,85 @@
+let Transition = React.addons.CSSTransitionGroup;
+
+
 Other = React.createClass({
+  getInitialState() {
+    return {
+      isShowModal: false
+    }
+  },
+  
+  showIonModal( value ) {
+    this.setState( {isShowModal: value} );
+  },
+
   render() {
-    return <h1>other</h1>
+
+    console.log("Other(), render(), this.state.isShowModal" + this.state.isShowModal);
+
+    return (
+      <span>
+        <h1>other</h1>
+
+        <ConversationSubheader showIonModal={this.showIonModal} />
+
+        {this.state.isShowModal ? <Backdrop /> : false}
+
+        <Transition transitionName="modal">
+          {
+            this.state.isShowModal ? 
+              <myIonModal 
+                tab="Find friends" 
+                content={"Stuff"} 
+                showIonModal={this.showIonModal} /> 
+            : 
+              null
+          }
+        </Transition>
+
+      </span>
+    );
   }
 });
 
+
 ConversationSubheader = React.createClass({
+
+  handleClick() {
+    console.log("ConversationSubheader(), handleClick()");
+    
+    this.props.showIonModal( true );
+  },
+
   render() {
     return (
       <div className="bar bar-subheader">
         <button className="button button-clear button-positive">Edit</button>
         <h2 className="title"></h2>
-        <button className="button button-icon icon ion-compose" onClick={this.props.ionModal.bind(null, "Find friends", <NewConversationModal />)}></button>
+        <button 
+          className="button button-icon icon ion-compose" 
+          onClick={ this.handleClick }>
+        </button>
       </div>
     )
   }
 })
+
+
+myIonModal = React.createClass({
+  render() {
+    return (
+        <IonModal modalContent={this.props.content}>
+          <div className="h1 title">{this.props.tab}</div>
+          <button 
+            onClick={ () => {this.props.showIonModal(false) } } 
+            className="button button-icon active">
+              <i className="icon ion-ios-close-empty"></i>
+          </button>
+        </IonModal>
+    )
+  }
+})
+
 
 NewConversationModal = React.createClass({
   mixins: [ReactMeteorData],
@@ -28,11 +93,13 @@ NewConversationModal = React.createClass({
     }
   },
   render() {
-    console.log(this.data.usernamesLoading);
-    console.log(this.data.usernames);
+    console.log("NewConversationModal(), this.data.usernamesLoading = " + this.data.usernamesLoading);
+    console.log("NewConversationModal(), this.data.usernames = " + this.data.usernames);
+    
     if (this.data.usernamesLoading) {
       return <AppLoading />
     }
+
     return (
       <div className="list">
         this.data.usernames.map((user) => {
